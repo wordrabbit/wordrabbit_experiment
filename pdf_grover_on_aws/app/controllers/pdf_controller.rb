712c8ec4fefaffc20d57adb710c7a66350c3b965
tsuyoshi fukuzawa <tsuyoshi.fukuzawa@gmail.com>
2023-11-30 05:17:50 +0900

    update

--- pdf_grover_on_aws/app/controllers/pdf_controller.rb
+++ pdf_grover_on_aws/app/controllers/pdf_controller.rb
@@ -1,11 +1,11 @@
 class PdfController < ApplicationController
   def index
-    controller = ActionController::Base.new
-    html = controller.render_to_string(template: 'pdf/index', layout: 'pdf')
-    pdf = Grover.new(html).to_pdf
     respond_to do |format|
       format.html
       format.pdf do
+        controller = ActionController::Base.new
+        html = controller.render_to_string(template: 'pdf/index', layout: 'pdf')
+        pdf = Grover.new(html).to_pdf
         send_data(pdf, filename: 'sample.pdf', type: 'application/pdf')
       end
     end
